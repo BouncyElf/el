dc160d8e43b778a222a2c7880e2c3a7bdd9ef2ec
BouncyElf <liuqiangqiang@starlinke.com>
2021-04-07 15:38:57 +0800

    style: add blank

--- conf.go
+++ conf.go
@@ -7,25 +7,35 @@ import (
 type Conf struct {
 	// Prefix is the prefix of a log.
 	Prefix string
+
 	// The logs are write to these outputs.
 	Outs []io.Writer
+
 	// Value is the value, that every log will include.
 	Value map[string]interface{}
+
 	// Format is the log format.
 	Format string
+
 	// AddCaller specific should the log add caller.
 	AddCaller bool
+
 	// CallerSkip specific the runtime.Caller(skip) skip number.
 	CallerSkip int
+
 	// AddTime specific should the log add time.
 	AddTime bool
+
 	// TimeFormat specific the log's time format.
 	// "unixnano" and "unix" indicates time.UnixNano() and time.Unix()
 	TimeFormat string
+
 	// NotPanic specific should the log panic after panic method.
 	NotPanic bool
+
 	// NotFatal specific should the log exit after panic method.
 	NotFatal bool
+
 	// LowestLevel specific the lowest log level.
 	// level less than LowestLevel will not be log.
 	LowestLevel level
